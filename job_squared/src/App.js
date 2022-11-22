edf43a16cdfcfcd13bacc0f6d21ece31dad92e2b
JCPenne <jackcpalmer@icloud.com>
2022-11-21 21:08:30 -0800

    front-end commit also first and only

--- job_squared/src/App.js
+++ job_squared/src/App.js
@@ -1,11 +1,20 @@
+import { React, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    axios.get('/test').then(data => {
+      console.log(data);
+    });
+  }, []);
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Hello World!</h1>
     </div>
   );
 }
 
-export default App; 
+export default App;
