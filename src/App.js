6736d2e94af2a7c2f96b6e0aac739d42574a2c63
JCPenne <jackcpalmer@icloud.com>
2022-11-22 15:56:02 -0800

    add login form and remove axios.get from App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,40 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    axios.get('/').then(data => {
-      console.log(data.data);
-    });
-  }, []);
-
   return (
     <div className='App'>
       <h1>This is the Landing Page!</h1>
+      <form>
+        <div className='email'>
+          <label
+            className='form__label'
+            for='email'
+          >
+            Email{' '}
+          </label>
+          <input
+            type='email'
+            id='email'
+            className='form__input'
+            placeholder='Email'
+          />
+        </div>
+        <div className='password'>
+          <label
+            className='form__label'
+            for='password'
+          >
+            Password{' '}
+          </label>
+          <input
+            className='form__input'
+            type='password'
+            id='password'
+            placeholder='Password'
+          />
+        </div>
+        <button>Sign in</button>
+      </form>
     </div>
   );
 }
