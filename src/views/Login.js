2c5559f52b27e58d6e6a26f4156885c844b7d819
JCPenne <jackcpalmer@icloud.com>
2022-11-23 15:39:04 -0800

    initial commit

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,58 +1,52 @@
-import { useState } from "react";
-import axios from "axios";
+import { useState } from 'react';
+import axios from 'axios';
 
+import FormField from '../components/FormField';
 
 export default function Login() {
   //TO DO Have yet to implement function
   const userLogin = () => {
-    axios.post('/' /*or 'http://localhost...'*/, {
-      email: email, password: password
-    }).then((response) => {
-      console.log(response);
-    })
+    axios
+      .post('/' /*or 'http://localhost...'*/, {
+        email: email,
+        password: password,
+      })
+      .then(response => {
+        console.log(response);
+      });
   };
 
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <form>
       <div className='email'>
-        <label
-          className='form__label'
-        >
-          Email{' '}
-        </label>
+        <label className='form__label'>Email </label>
         <input
           type='email'
           id='email'
           className='form__input'
           placeholder='Email'
           // onchange event to grab the value entered
-          onChange={(e) => { setEmail(e.target.value) }}
+          onChange={e => {
+            setEmail(e.target.value);
+          }}
         />
       </div>
       <div className='password'>
-        <label
-          className='form__label'
-        >
-          Password{' '}
-        </label>
+        <label className='form__label'>Password </label>
         <input
           className='form__input'
           type='password'
           id='password'
           placeholder='Password'
-          onChange={(e) => { setPassword(e.target.value) }}
+          onChange={e => {
+            setPassword(e.target.value);
+          }}
         />
       </div>
       <button>Sign in</button>
     </form>
   );
-};
-
-
-
-
-
+}
