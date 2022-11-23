d16f6b34ac14fe6f9eb6869a1d50ea860ba410ff
JacquelynSH <elpatchaembroidery@gmail.com>
2022-11-22 19:39:41 -0800

    Create login component and render within App

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
 import { React, useEffect } from 'react';
 import axios from 'axios';
 
+import Login from './views/Login';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <h1>This is the Landing Page!</h1>
-      <form>
-        <div className='email'>
-          <label
-            className='form__label'
-            for='email'
-          >
-            Email{' '}
-          </label>
-          <input
-            type='email'
-            id='email'
-            className='form__input'
-            placeholder='Email'
-          />
-        </div>
-        <div className='password'>
-          <label
-            className='form__label'
-            for='password'
-          >
-            Password{' '}
-          </label>
-          <input
-            className='form__input'
-            type='password'
-            id='password'
-            placeholder='Password'
-          />
-        </div>
-        <button>Sign in</button>
-      </form>
+      <Login />
     </div>
   );
 }
