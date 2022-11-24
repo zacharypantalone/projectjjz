f493abac6363c7da1ea8e4a89a8bb174132d4151
Jack <JackcPalmer@iCloud.com>
2022-11-24 11:35:40 -0800

    Feature/user auth (#16)
    * initial commit
    
    * add formfield component to landing page view
    
    * add post to dashboard route on front end, ensure it receives res from server

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -3,15 +3,6 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 export default function Dashboard() {
-  const [] = useState([
-  ]);
-
-  useEffect(() => {
-    axios.get(`/dashboard`).then(data => {
-      console.log();
-    });
-  }, []);
-
   return (
     <div id='dashboard-page'>
       <p>This is the dashboard page</p>
