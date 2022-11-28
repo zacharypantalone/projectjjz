542c6fe6cd59a9959511cacdbe404b630bb93e8a
JacquelynSH <elpatchaembroidery@gmail.com>
2022-11-27 16:28:32 -0800

    Create user appointment component for display in dashboard

---
+++ src/components/UserAppointments.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/UserAppointments.scss'
+
+const UserAppointments = () => {
+  return (
+    <div className='user-appointments-component'>
+      <h2>Upcoming appointments</h2>
+      <h4>Thursday, Sept 4th, 11am</h4>
+      <h4>Thursday, Sept 4th, 11am</h4>
+      <h4>Thursday, Sept 4th, 11am</h4>
+    </div>
+  )
+}
+
+export default UserAppointments
