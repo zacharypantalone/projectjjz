0373dde6a56c5fa255b11ef2a16b5a31966236b4
JacquelynSH <elpatchaembroidery@gmail.com>
2022-11-27 16:31:59 -0800

    Move tile functionality to CareerTile component/cleanup file

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,55 +1,40 @@
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { careerList } from '../careerList';
+import CareerTile from '../components/CareerTile';
+
+import '../styles/Dashboard.css';
+import UserAppointments from '../components/UserAppointments';
+import backgroundImage from '../assets/background-image.jpg';
+import Logo from '../assets/Logo.svg';
 
 
-
-export default function Dashboard() {
-  //Quiz results would give you the Career ex. tech
-  const [quizResults, setQuizResults] = useState();
-  //Job would give you the jobs that fall within the Career category ex. creative coding, web dev
-  const [jobs, setJobs] = useState([
-    { job1: '' },
-    { job2: '' },
-    { job3: '' }
-  ]);
-
-
-  const handleLogoutClick = (event) => {
-    event.preventDefault()
-  }
-
-  useEffect(() => {
-    const currentUser = 1
-    axios.get(`/quizresults?userId=${currentUser}`)
-      .then(res => {
-        console.log(res)
-        setJobs(
-          [
-            { job: res.data[0].recommendation_1 },
-            // { title: res.data[0].title}
-            { job: res.data[0].recommendation_2 },
-            { job: res.data[0].recommendation_3 },
-          ])
-      }).then(console.log(jobs))
-  }, []);
-
-
+const Dashboard = () => {
   return (
+    <main>
 
-
-    <div>
-      {jobs.map(job =>
-        <p>{job.job}</p>
-  // <CareerTile
-        //   key={job.id}
-        //   img={job.img}
-        //   title={job.title}
-        //   body={job.body}
-        // />
-        )}
+    <div className='dashboard'>
+    <img
+        className='Logo'
+        height={200}
+        width={100}
+        src={Logo}
+        alt='Logo SVG'
+      />
+      <h1>Welcome user! You are one step closer to a brighter future! </h1>
+      <h3>Quiz results.....</h3>
+      <CareerTile />
+      <UserAppointments />
     </div>
-
+    <img
+        className='background-image'
+        src={backgroundImage}
+      />
+    </main>
   )
 }
+
+export default Dashboard
+
 
