1727e15d8c05afd15865f4e4b923e8783b4226c1
JacquelynSH <elpatchaembroidery@gmail.com>
2022-11-28 12:10:54 -0800

    Remove unneeded import statement

--- src/components/CareerInfo.js
+++ src/components/CareerInfo.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useFetcher, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { careerList } from '../careerList'
 
