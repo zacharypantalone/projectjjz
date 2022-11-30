f31ae5870ee445691cf0eb1589cca4fba8263a7a
JCPenne <jackcpalmer@icloud.com>
2022-11-30 15:06:16 -0800

    change navigate on the retake quiz button to go to quiz

--- src/components/QuizResults.js
+++ src/components/QuizResults.js
@@ -10,39 +10,53 @@ const QuizResults = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-
     axios.get(`/quizresults`).then(res => {
-      setJobs(
-        res.data
-      );
+      setJobs(res.data);
     });
   }, []);
 
   const handleRetake = () => {
-    axios.delete('/quizresults')
-    .then(navigate('/dashboard'))
-  }
+    axios.delete('/quizresults').then(navigate('/quiz'));
+  };
 
   return (
     <article className='career-tiles'>
-      <h3>Now that you have taken our quiz you're one step closer to a better future!</h3>
+      <h3>
+        Now that you have taken our quiz you're one step closer to a better
+        future!
+      </h3>
       <h3>Here are the results from your last quiz</h3>
       {jobs.map((job, index) => (
         <div className='career-tile'>
           <div>
-            <img src={job.img} alt="img" width={150}></img>
+            <img
+              src={job.img}
+              alt='img'
+              width={150}
+            ></img>
             <p>{job.title}</p>
             <p>{job.body}</p>
           </div>
-          <button onClick={() => {
-            navigate(`/career/${index + 1}`);
-          }}>Click here for more info!
+          <button
+            onClick={() => {
+              navigate(`/career/${index + 1}`);
+            }}
+          >
+            Click here for more info!
           </button>
         </div>
       ))}
       <div>
-        <p>If you would like to retake the quiz for any reason you can click below</p>
-        <button type='button' onClick={handleRetake}>Retake Quiz</button>
+        <p>
+          If you would like to retake the quiz for any reason you can click
+          below
+        </p>
+        <button
+          type='button'
+          onClick={handleRetake}
+        >
+          Retake Quiz
+        </button>
       </div>
     </article>
   );
