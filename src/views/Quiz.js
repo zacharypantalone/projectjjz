46b16ae196c2fb249dfe70cbc7b9e5f060de6a3b
Zachary Pantalone <52625310+zacharypantalone@users.noreply.github.com>
2022-11-30 13:39:17 -0500

    quiz front end working (#26)

--- src/views/Quiz.js
+++ src/views/Quiz.js
@@ -1,96 +1,155 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { initialQuestion, questions1, questions2 } from './QuizQuestions';
 
 export default function Quiz() {
-  const [quizzes, setQuizzes] = useState([
-  ]);
 
-  // ^^ DO WE NEED THIS?
+
+  const [questions, setQuestions] = useState([])
+  const [questionCount, setQuestionCount] = useState(0)
+  const [quizResults, setQuizResults] = useState([1,2,3])
+
+  const navigate = useNavigate()
 
   useEffect(() => {
-    axios.get(`/quiz`).then(data => {
-      console.log();
-    });
+    axios.get('/quizquestions')
+    .then(res => {
+      setQuestions(res.data)
+    })
   }, []);
 
-  
-  const [currentQuestion, setCurrentQuestion] = useState(0)
-  const [isRemote, setIsRemote] = useState(undefined)
-  const [questions, setQuestions] = useState([])
-  const [passedInitialQuestion, setPassedInitialQuestion] = useState(false)
+const handleClick = (event) => {
+  event.preventDefault()
+  setQuestionCount(prevCount => prevCount + 1)
 
-  const handleAnswerButtonClick = (answerOption) => {
-    const nextQuestion = currentQuestion + 1;
-    setCurrentQuestion(nextQuestion);
+}
+const handleFinalClick = (event) => {
+  event.preventDefault()
+  axios.post('/quizresults', quizResults)
 
-    // CONSIDER VISUAL MODE HOOK
+  navigate('/dashboard');
+}
 
-    if (currentQuestion === 0 && !passedInitialQuestion) {
-      console.log(answerOption)
-      if (answerOption === "Remotely") {
-        setIsRemote(true);
-        setQuestions(questions1);
-        setCurrentQuestion(0)
-        setPassedInitialQuestion(true)
-        
-
-      } else {
-        setIsRemote(false);
-        setQuestions(questions2)
-        setCurrentQuestion(0)
-        setPassedInitialQuestion(true)
-      }
-    } 
-    
-    else {
-
-      if (nextQuestion < questions.length) {
-        setCurrentQuestion(nextQuestion);
-      } else {
-        alert("You're all set! Click here to head back to the dashboard for your results");
-        // AXIOS REQUEST TO DATABASE OF QUIZ RESULTS?
-      }
-    }
+const renderQuiz = () => {
+  if (questionCount !== questions.length ) {
+    return (
+      <div>
+        {questions[questionCount].question}
+        <button onClick={handleClick}>{questions[questionCount].answer_one}</button>
+        <button onClick={handleClick}>{questions[questionCount].answer_two}</button>
+      </div>
+    )
+  } 
+  else {
+    return (
+      <div>
+        <button onClick={handleFinalClick}>
+          You're all set! Click here to go back to your dashboard and see the results
+        </button>
+      </div>
+    )
   }
+}
+  
+return (
+  <>
+    {questions.length > 0 ? <div>{renderQuiz()}</div> : <div>Questions not populated yet</div>}
+  </>
+  )
+  }
+    
+    
+    
 
-  console.log(isRemote);
+
+    
+    
   
 
-	return (
-		<div id='Quiz-page'>
-			
-        {currentQuestion === 0 && !passedInitialQuestion ?
-        <div className='answer-section'>
-          <div className='question-text'>{initialQuestion[currentQuestion].questionText}</div>
-        {initialQuestion[currentQuestion].answerOptions.map((answerOptions, index) => (
-          <button onClick={() => handleAnswerButtonClick(answerOptions.answerText)}>{answerOptions.answerText}</button>
-        ))}
-      </div> 
-      
-      :
-      
-      <div className='answer-section'>
-      <div className='question-text'>{questions[currentQuestion].questionText}</div>
-    {questions[currentQuestion].answerOptions.map((answerOptions, index) => (
-      <button onClick={() => handleAnswerButtonClick(answerOptions.answerText)}>{answerOptions.answerText}</button>
-    ))}
+
+  
+//   const [currentQuestion, setCurrentQuestion] = useState(0)
+//   const [isRemote, setIsRemote] = useState(undefined)
+  
+//   const [passedInitialQuestion, setPassedInitialQuestion] = useState(false)
+//   const [quizComplete, setQuizComplete] = useState(false) // OR THIS NEEDS TO BE SET TO UNDEFINED
+
+//   const handleAnswerButtonClick = (answerOption) => {
+//     const nextQuestion = currentQuestion + 1;
+//     setCurrentQuestion(nextQuestion);
+
+//     // CONSIDER VISUAL MODE HOOK
+
+//     if (currentQuestion === 0 && !passedInitialQuestion) {
+//       console.log(answerOption)
+//       if (answerOption === "Remotely") {
+//         // setIsRemote(true);
+//         setQuestions(
+//           axios.get
+//           // AXIOS REQUEST
+//         );
+//         setCurrentQuestion(0) // PROBABLY DON'T NEED THIS LINE
+//         // setPassedInitialQuestion(true) // AND THIS EITHER
         
-  </div>}
-					<div className='question-section'>
-						<div className='question-count'>
-              {/* WE NEED TO FIGURE OUT HOW TO TRACK WHICH QUESTION WE ARE ON THE LINE BELOW  */}
-							<span>Question{currentQuestion + 1}</span>/{initialQuestion.length} 
-						</div>
+
+//       } else {
+//         // setIsRemote(false);
+//         setQuestions(questions2)
+//         setCurrentQuestion(0)
+//         // setPassedInitialQuestion(true)
+//       }
+//     } 
+    
+//     else {
+
+//       if (nextQuestion < questions.length) {
+//         setCurrentQuestion(nextQuestion);
+//       } else {
+//         alert("You're all set! Click here to head back to the dashboard for your results");
+//         setQuizComplete(true,
+//           // axios.post ??
+//           );
+        
+//       }
+//     }
+//   }
+
+//   console.log(isRemote);
+  
+
+// 	return (
+// 		<div id='Quiz-page'>
+			
+//         {currentQuestion === 0 && !passedInitialQuestion ?
+//         <div className='answer-section'>
+//           <div className='question-text'>{initialQuestion[currentQuestion].questionText}</div>
+//         {initialQuestion[currentQuestion].answerOptions.map((answerOptions, index) => (
+//           <button onClick={() => handleAnswerButtonClick(answerOptions.answerText)}>{answerOptions.answerText}</button>
+//         ))}
+//       </div> 
+      
+//       :
+      
+//       <div className='answer-section'>
+//       <div className='question-text'>{questions[currentQuestion].questionText}</div>
+//     {questions[currentQuestion].answerOptions.map((answerOptions, index) => (
+//       <button onClick={() => handleAnswerButtonClick(answerOptions.answerText)}>{answerOptions.answerText}</button>
+//     ))}
+        
+//   </div>}
+// 					<div className='question-section'>
+// 						<div className='question-count'>
+//               {/* WE NEED TO FIGURE OUT HOW TO TRACK WHICH QUESTION WE ARE ON THE LINE BELOW  */}
+// 							<span>Question{currentQuestion + 1}</span>/{initialQuestion.length} 
+// 						</div>
 						
-					</div>
+// 					</div>
 					
 				
 			
 		
-		</div>
-	);
+// 		</div>
+// 	);
 
 
-}
+// }
--- src/views/QuizQuestions.js
+++ src/views/QuizQuestions.js
@@ -2,51 +2,51 @@
 
 
 
-export const initialQuestion = [
-  {
-    questionText: "Would you prefer to work on-site or remotely?",
-    answerOptions: [
-     {answerText: "On-site", isCorrect: false},
-     {answerText: "Remotely", isCorrect: false} 
-    ]
-  }
-]
+// export const initialQuestion = [
+//   {
+//     questionText: "`Would you prefer to work on-site or remotely?`",
+//     answerOptions: [
+//      {answerText: "On-site", isCorrect: false},
+//      {answerText: "Remotely", isCorrect: false} 
+//     ]
+//   }
+// ]
 
 
-// how about this? { remotely: { questions: [] } , on_site: { questions: []} }
+// // how about this? { remotely: { questions: [] } , on_site: { questions: []} }
 
 
-export const questions1 = [
-  {
-    questionText: 'What is your strongest skill?',
-    answerOptions: [
-      { answerText: 'Logical thinking', /*SELECTION */ isCorrect: false },
-      { answerText: 'Communication', isCorrect: false }
+// export const questions1 = [
+//   {
+//     questionText: 'What is your strongest skill?',
+//     answerOptions: [
+//       { answerText: 'Logical thinking', /*SELECTION */ isCorrect: false },
+//       { answerText: 'Communication', isCorrect: false }
       
-    ],
-  },
-  {
-    questionText: 'Do you prefer manipulating data, or creative tasks?',
-    answerOptions: [
-      { answerText: 'I love data!', isCorrect: false },
-      { answerText: "I love creating!", /*SELECTION */ isCorrect: true }
-    ],
-  },
-  {
-    questionText: 'Would you prefer to create application for users or tools for professionals?',
-    answerOptions: [
-      { answerText: 'I want to create apps for the everyday joe.', /*SELECTION */ isCorrect: true },
-      { answerText: 'I want to create apps for all my fellow geeks.', isCorrect: false }
-    ],
-  },
-  {
-    questionText: 'In your day-to-day, would you enjoy working with informational-based, or visual-based programs?',
-    answerOptions: [
-      { answerText: "I'm more of and informational person", /*SELECTION */ isCorrect: false },
-      { answerText: "I'm more of a visual person", isCorrect: false },
-    ],
-  },
-];
+//     ],
+//   },
+//   {
+//     questionText: 'Do you prefer manipulating data, or creative tasks?',
+//     answerOptions: [
+//       { answerText: 'I love data!', isCorrect: false },
+//       { answerText: "I love creating!", /*SELECTION */ isCorrect: true }
+//     ],
+//   },
+//   {
+//     questionText: 'Would you prefer to create application for users or tools for professionals?',
+//     answerOptions: [
+//       { answerText: 'I want to create apps for the everyday joe.', /*SELECTION */ isCorrect: true },
+//       { answerText: 'I want to create apps for all my fellow geeks.', isCorrect: false }
+//     ],
+//   },
+//   {
+//     questionText: 'In your day-to-day, would you enjoy working with informational-based, or visual-based programs?',
+//     answerOptions: [
+//       { answerText: "I'm more of and informational person", /*SELECTION */ isCorrect: false },
+//       { answerText: "I'm more of a visual person", isCorrect: false },
+//     ],
+//   },
+// ];
 
 
 
@@ -57,41 +57,41 @@ export const questions1 = [
 
 
 
-export const questions2 = [ /*🛑 WE STILL NEED TO FIGURE OUR SECOND MVP IN THIS SERIES OF QUESTIONS 🛑*/
-  {
-    questionText: 'What is the capital of France?',
-    answerOptions: [
-      { answerText: 'New York', isCorrect: false },
-      { answerText: 'London', isCorrect: false },
-      { answerText: 'Paris', isCorrect: true },
-      { answerText: 'Dublin', isCorrect: false },
-    ],
-  },
-  {
-    questionText: 'Who is CEO of Tesla?',
-    answerOptions: [
-      { answerText: 'Jeff Bezos', isCorrect: false },
-      { answerText: 'Elon Musk', isCorrect: true },
-      { answerText: 'Bill Gates', isCorrect: false },
-      { answerText: 'Tony Stark', isCorrect: false },
-    ],
-  },
-  {
-    questionText: 'The iPhone was created by which company?',
-    answerOptions: [
-      { answerText: 'Apple', isCorrect: true },
-      { answerText: 'Intel', isCorrect: false },
-      { answerText: 'Amazon', isCorrect: false },
-      { answerText: 'Microsoft', isCorrect: false },
-    ],
-  },
-  {
-    questionText: 'How many Harry Potter books are there?',
-    answerOptions: [
-      { answerText: '1', isCorrect: false },
-      { answerText: '4', isCorrect: false },
-      { answerText: '6', isCorrect: false },
-      { answerText: '7', isCorrect: true },
-    ],
-  },
-];
+// export const questions2 = [ /*🛑 WE STILL NEED TO FIGURE OUR SECOND MVP IN THIS SERIES OF QUESTIONS 🛑*/
+//   {
+//     questionText: 'What is the capital of France?',
+//     answerOptions: [
+//       { answerText: 'New York', isCorrect: false },
+//       { answerText: 'London', isCorrect: false },
+//       { answerText: 'Paris', isCorrect: true },
+//       { answerText: 'Dublin', isCorrect: false },
+//     ],
+//   },
+//   {
+//     questionText: 'Who is CEO of Tesla?',
+//     answerOptions: [
+//       { answerText: 'Jeff Bezos', isCorrect: false },
+//       { answerText: 'Elon Musk', isCorrect: true },
+//       { answerText: 'Bill Gates', isCorrect: false },
+//       { answerText: 'Tony Stark', isCorrect: false },
+//     ],
+//   },
+//   {
+//     questionText: 'The iPhone was created by which company?',
+//     answerOptions: [
+//       { answerText: 'Apple', isCorrect: true },
+//       { answerText: 'Intel', isCorrect: false },
+//       { answerText: 'Amazon', isCorrect: false },
+//       { answerText: 'Microsoft', isCorrect: false },
+//     ],
+//   },
+//   {
+//     questionText: 'How many Harry Potter books are there?',
+//     answerOptions: [
+//       { answerText: '1', isCorrect: false },
+//       { answerText: '4', isCorrect: false },
+//       { answerText: '6', isCorrect: false },
+//       { answerText: '7', isCorrect: true },
+//     ],
+//   },
+// ];
