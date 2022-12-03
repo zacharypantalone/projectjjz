142aeca60145a0e61faa926178af76c03349bf26
JCPenne <jackcpalmer@icloud.com>
2022-12-02 18:32:37 -0800

    set appointments conditionally for each mentor click

--- src/views/Schedule.js
+++ src/views/Schedule.js
@@ -8,15 +8,10 @@ export default function Schedule() {
   const [quizResults, setquizResults] = useState({});
   const [mentors, setMentors] = useState({});
   const [filteredMentors, setfilteredMentors] = useState({});
-  const [currentMentor, setcurrentMentor] = useState('');
+  const [currentMentor, setcurrentMentor] = useState({});
   const [day, setDay] = useState([]);
-  const [time, setTime] = useState([
-    { Time: '9am' },
-    { Time: '10am' },
-    { Time: '11am' },
-    { Time: '12pm' },
-    { Time: '1pm' },
-  ]);
+  const [time, setTime] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
   useEffect(() => {
     axios.get(`/quizresults`).then(res => {
@@ -29,7 +24,6 @@ export default function Schedule() {
       setDay(res.data);
     });
     axios.get('/times').then(res => {
-      console.log(res.data);
       setTime(res.data);
     });
   }, []);
@@ -39,13 +33,24 @@ export default function Schedule() {
       return x.job_id === id;
     });
     setfilteredMentors(filteredMentors);
+    setcurrentMentor({});
   };
 
   const mentorClick = (id, name) => {
-    setcurrentMentor(name);
+    setcurrentMentor({ id, name });
+    axios
+      .get('/appointments', { params: { id: currentMentor.id } })
+      .then(res => {
+        setAppointments(res.data);
+      });
   };
 
   const dayClick = () => {};
+
+  // console.log('appointments', appointments);
+  // console.log('times', time);
+  // console.log('current Mentor', currentMentor);
+  // console.log('filtered Mentors', filteredMentors);
 
   return (
     <div id='schedule'>
@@ -56,9 +61,7 @@ export default function Schedule() {
         alt='background'
       />
       <section className='career-recommendations'>
-        <h3>
-          Select a career to see which mentors are available to chat with
-        </h3>
+        <h3>Select a career to see which mentors are available to chat with</h3>
         <section className='career-buttons'>
           {quizResults.length > 0 ? (
             quizResults.map(result => {
@@ -77,50 +80,88 @@ export default function Schedule() {
           )}
         </section>
         <section className='mentor-list'>
-          {filteredMentors.length > 0 ? (
-            filteredMentors.map(result => {
-              return (
-                <section className='mentor-tile'>
-                  <img
-                    className='mentor-headshot'
-                    src={result.headshot}
-                  />
-                  <p>{result.blurb}</p>
-                  <button
-                    className='main-button'
-                    onClick={() => mentorClick(result.id, result.first_name)}
-                  >
-                    Book a chat with {result.first_name}
-                  </button>
-                </section>
-              );
-            })
-          ) : ''}
+          {filteredMentors.length > 0
+            ? filteredMentors.map(result => {
+                return (
+                  <section className='mentor-tile'>
+                    <img
+                      className='mentor-headshot'
+                      src={result.headshot}
+                    />
+                    <p>{result.blurb}</p>
+                    <button
+                      className='main-button'
+                      onClick={() => mentorClick(result.id, result.first_name)}
+                    >
+                      Book a chat with {result.first_name}
+                    </button>
+                  </section>
+                );
+              })
+            : ''}
         </section>
       </section>
-      {!filteredMentors.length > 0 ? <h3>No Career Selected</h3> :
-      <section id='schedule-tile'>
-        {currentMentor.length > 0 ? <h3>{currentMentor}'s Availability</h3> : <h3>Select a Mentor to see their Availability</h3>}
-        <section className='day-and-time'>
-          <section className='day-tiles'>
-            {day.map(x => {
-              return (
-                <button
-                  onClick={dayClick}
-                  className='day-tile secondary-button'
-                >
-                  {x.day}
-                </button>
-              );
-            })}
-          </section>
-          <section className='time-tiles'>
-            {time.map(x => {
-              return <div className='time-tile'>{x.time}</div>;
-            })}
+      {!filteredMentors.length > 0 ? (
+        <h3>No Career Selected</h3>
+      ) : (
+        <section id='schedule-tile'>
+          {currentMentor.name ? (
+            <h3>{currentMentor.name}'s Availability</h3>
+          ) : (
+            <h3>Select a Mentor to see their Availability</h3>
+          )}
+          <section className='day-and-time'>
+            <section className='day-tiles'>
+              {day.map(x => {
+                return (
+                  <button
+                    onClick={dayClick}
+                    className='day-tile secondary-button'
+                  >
+                    {x.day}
+                  </button>
+                );
+              })}
+            </section>
+            <section className='time-tiles'>
+              {time.map(t => {
+                if (appointments.some(a => a.time_id === t.id)) {
+                  return (
+                    <div
+                      className='time-tile-full'
+                      key={t.time}
+                    >
+                      {t.time}
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div
+                      className='time-tile-available'
+                      key={t.time}
+                    >
+                      {t.time}
+                    </div>
+                  );
+                }
+              })}
+            </section>
           </section>
         </section>
-      </section>}
+      )}
     </div>
   );
 }
+
+// {
+//   time.map(x => {
+//     return (
+//       <div
+//         className='time-tile'
+//         key={x.time}
+//       >
+//         {x.time}
+//       </div>
+//     );
+//   });
+// }
