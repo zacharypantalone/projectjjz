e0719390a39ae9fb91f47fb5b04da4f853b8de77
Jackie H <99279776+JacquelynSH@users.noreply.github.com>
2022-12-04 11:19:07 -0800

    Feature dashboard styling (#38)
    * Improve styling for quiz results and career page
    
    * Minor CSS changes
    
    * Improve styling for landing page
    
    * Adjust styles for quiz not taken for better prompting
    
    * Add drop down to all pages/move logout functionality to own component
    
    * Style register form/touch up related components and styles

--- src/components/QuizResults.js
+++ src/components/QuizResults.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import '../styles/QuizResults.scss';
+import backEndDev from '../assets/back-end-dev.png';
+import frontEndDev from '../assets/front-end-dev.png';
+import creativeCoder from '../assets/creative-coding.png';
 import { useNavigate } from 'react-router';
 
 const QuizResults = () => {
   const [jobs, setJobs] = useState([]);
-
+  const jobImages = [creativeCoder, frontEndDev, backEndDev];
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,37 +22,39 @@ const QuizResults = () => {
     axios.delete('/quizresults').then(navigate('/quiz'));
   };
 
-  return (      
-  <article>
-  <h3>
-    Now that you have taken our quiz you're one step closer to a better
-    future!
-  </h3>
-  <h3>Here are the results from your last quiz</h3>
-    <div className='career-tiles'>
-    
-      {jobs.map((job, index) => (
-        <div className='career-tile'>
-          <div>
+  return (
+
+    <article>
+      <div className='upper-quiz-results'>
+      <h3 >
+        Now that you have taken our quiz you're one step closer to a better
+        future!
+      </h3>
+      <h1>Here are the results from your last quiz.....</h1>
+      </div>
+      <div className='career-tiles'>
+        {jobs.map((job, index) => (
+          <div className='career-tile'>
             <img
-              src={job.img}
+              src={jobImages[index]}
               alt='img'
-              width={150}
-            ></img>
-            <p>{job.title}</p>
-            <p>{job.body}</p>
+              width={175}
+            />
+            <div>
+              <h3>{job.title}</h3>
+              <p className='career-body'>{job.body}</p>
+            </div>
+            <button
+              onClick={() => {
+                navigate(`/career/${index + 1}`);
+              }}
+            >
+              Click here for more info!
+            </button>
           </div>
-          <button
-            onClick={() => {
-              navigate(`/career/${index + 1}`);
-            }}
-          >
-            Click here for more info!
-          </button>
-        </div>
-      ))}
-    </div>
-    <div>
+        ))}
+      </div>
+      <div>
         <h3 className='quiz-retake-prompt'>
           If you would like to retake the quiz for any reason you can click
           below
--- src/views/Career.js
+++ src/views/Career.js
@@ -2,46 +2,66 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
+import '../styles/career.scss';
 import backgroundImage from '../assets/background-image.jpg';
-
+import backEndDev from '../assets/back-end-dev.png';
+import frontEndDev from '../assets/front-end-dev.png';
+import creativeCoder from '../assets/creative-coding.png';
+import DropDownMenu from '../components/DropDownMenu';
 
 export default function Career() {
 
   const [career, setCareer] = useState({});
-
+  const jobImages = [creativeCoder, frontEndDev, backEndDev];
   const { jobId } = useParams();
 
 
   useEffect(() => {
-    axios.get(`/careerinfo/${jobId}`).then(res => {setCareer(res.data)} )
-  }, [] )
+    axios.get(`/careerinfo/${jobId}`).then(res => { setCareer(res.data) })
+  }, [])
 
   console.log("HERE", career)
   return (
-    <div>
-        <img
+    <div className='career-component'>
+      <DropDownMenu />
+      <img
         className='background-image'
         src={backgroundImage}
       />
-      <article>
-      {Object.keys(career).length !==0 && <div>
-      <img src={career.job.img} alt="img" width={400}/> 
-      <div style={{ color: "white" }}>{career.job.title}</div>
-      <div style={{ color: "white" }}>{career.job.body}</div>
-      <div style={{ color: "white" }}>Average Salary: {career.job.average_salary}</div>
-      <div style={{ color: "white" }}>Salary Range: {career.job.salary_range}</div>
-      <div style={{ color: "white" }}>Training Needed: {career.job.training}</div>
-      <div style={{ color: "white" }}>Necessary Skills: {career.job.skills}</div>
-      <h3>Related Articles: </h3>
-      {career.article.map((article) => (
-        <a href={article.article}>{article.article}</a>
-      ))}
-      <h3>Learning Links:</h3>
-      {career.learning.map((learning) => (
-        <a href={learning.learning}>{learning.learning}</a>
-      ))}
-      </div>}
+      <article className='career-info-container'>
+        <div className='career-info'>
+          {Object.keys(career).length !== 0 && <div>
+            <div className='career-image-title'>
+              <img src={jobImages[jobId - 1]} alt="img" width={325} />
+              <h1>{career.job.title}</h1>
+            </div>
+            <h4 >{career.job.body}</h4>
+
+            <div className='career-info-breakdown'><strong className='bold-title'>Average Salary:</strong>{career.job.average_salary}</div>
+            <div className='career-info-breakdown'><strong className='bold-title'>Salary Range: </strong>{career.job.salary_range}</div>
+            <div className='career-info-breakdown'><strong className='bold-title'>Training Needed: </strong>{career.job.training}</div>
+            <div className='career-info-breakdown'><strong className='bold-title'>Necessary Skills: </strong>{career.job.skills}</div>
+
+
+            <h3>Related Articles: </h3>
+            <div className='links'>
+              {career.article.map((article) => (
+                <a href={article.article}>{article.article}</a>
+              ))}
+            </div>
+            <h3>Learning Links:</h3>
+            <div className='links'>
+              {career.learning.map((learning) => (
+                <a href={learning.link}>{learning.link}</a>
+              ))}
+            </div>
+          </div>}
+        </div>
       </article>
+      <img
+        className='background-image'
+        src={backgroundImage}
+      />
     </div>
   );
 }
