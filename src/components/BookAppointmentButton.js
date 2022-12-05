d22d3950a0ea063d4b9b4dd2415f104d854be3cb
JacquelynSH <elpatchaembroidery@gmail.com>
2022-12-04 16:00:32 -0800

    Make and style a book appointment button

---
+++ src/components/BookAppointmentButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useNavigate } from 'react-router'
+
+const BookAppointmentButton = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className='book-appointment-component'>
+      <h4>Speak with a profession in this field!</h4>
+      <button onClick={() => navigate('/schedule')}>Click here to book an appointment!</button>
+    </div>
+  )
+}
+
+export default BookAppointmentButton
