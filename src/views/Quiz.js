1eed29259b4c8d61cbcd7676a677ea01946d4245
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-12-05 16:24:06 -0500

    quiz styling

--- src/views/Quiz.js
+++ src/views/Quiz.js
@@ -32,9 +32,18 @@ export default function Quiz() {
   };
 
   const renderQuiz = () => {
+    
     if (questionCount === 0) {
       return (
+        
         <div className='quiz-main'>
+          <div className='bananas'>
+      <p className='quiz-intro-paragraph'>Welcome to the CareerSquared Quiz!
+        After answering these questions we will provide you with 3 Career Recommendations. 
+        In your Dashboard you will be able to see Links and Resources to help you begin your journey.
+        Also make sure to check out the Schedule page to schedule a meeting with a professional in one of your three careers
+    </p>
+    </div>  
         <div className='quiz-tile'>
           <div className='quiz-question'>
         {questions[questionCount].question}
@@ -89,18 +98,13 @@ export default function Quiz() {
         className='Careersquared'
         src={Careersquared}
       />
-      <div className='bananas'>
-      <p className='quiz-intro-paragraph'>Welcome to the CareerSquared Quiz!
-        After answering these questions we will provide you with 3 Career Recommendations. 
-        In your Dashboard you will be able to see Links and Resources to help you begin your journey.
-        Also make sure to check out the Schedule page to schedule a meeting with a professional in one of your three careers
-    </p>
+      
     {questions.length > 0 ? (
         <div>{renderQuiz()}</div>
       ) : (
         <div>Questions not populated yet</div>
       )}
-      </div>
+      
      
     </>
   );
